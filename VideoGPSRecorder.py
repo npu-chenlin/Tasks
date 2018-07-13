4e8534f7f9cd059376cd93117d13892591566621
npu-chenlin <31912177+npu-chenlin@users.noreply.github.com>
2018-07-13 10:52:22 +0800

    Add files via upload
    This prog is built for testting GPS & record GPS data while taking pics.
    Before using this , u should run VideoGPSRecorder.py --act testSerial --port <ur path by default /dev/ttyUSB0> --baud <ur baudrate by default 115200> to test if GPS is workable.Wait untill it stop searching...

---
+++ VideoGPSRecorder.py
@@ -0,0 +1,114 @@
+# coding=utf-8
+#This prog is built for testting GPS & record GPS data while taking pics.
+#Before using this , u should run VideoGPSRecorder.py --act testSerial --port <ur path by default /dev/ttyUSB0> --baud <ur baudrate by default 115200> to test if GPS is workable.Wait untill it stop searching...
+#
+
+
+import time,os
+import serial
+import cv2
+import requests,json
+import argparse
+import threading
+
+
+def getTimestampUTC(l):
+		t_ = time.gmtime(time.time())	
+		t = (t_.tm_year,t_.tm_mon,t_.tm_mday,int(l[1][:2]),int(l[1][2:4]),int(l[1][4:6]),t_.tm_wday,t_.tm_yday,t_.tm_isdst)
+		return time.mktime(t)+float(l[1][6:])
+
+def parse_GPGGA(l):
+	if(l[2]):
+		timestamp = getTimestampUTC(l)
+		lon = float(l[4][0:3])+float(l[4][3:])/60
+		lat = float(l[2][0:2])+float(l[2][2:])/60
+		#url='https://restapi.amap.com/v3/geocode/regeo?location='+lon+','+lat+'&key=a317cca0f0706fadc03bc0761eaa9db3'
+		#cont=requests.get(url)
+		#cont=json.loads(cont.content)
+		#addr=cont["regeocode"]["formatted_address"].decode('utf8')
+		#info["addr"]=addr
+		sat_num=l[7]
+		HDOP=l[8]
+		info=[timestamp,lon,lat,sat_num,HDOP]	
+		return info 
+	else:
+		return 'searching...'
+
+def read_GPS(gps):
+	l=gps.readline().split(',')
+	while(not (l[0]=='$GPGGA' or l[0]=='$GNGGA')):
+		l=gps.readline().split(',')
+	r=parse_GPGGA(l)
+	if(not type(r) == str):
+		a=[str(i)+' ' for i in r]
+		a[-1]+="\n"
+		return a
+	else:
+		return r
+
+def dataToTxt(cfg):
+	path='GPS_data.txt'
+	count=0
+	with open(path,'a') as f:
+		while count<100:
+			r=read_GPS(gps)
+			print(r)
+			if(type(r) == list):
+				f.writelines(read_GPS(gps))
+			count+=1
+	return 0
+
+def captureVideo(videoCapture):
+	if(not os.path.exists('./pic')):
+		os.mkdir('./pic')
+	sucess,frame=videoCapture.read()
+	while(sucess):
+		sucess,frame=videoCapture.read()
+		img=cv2.resize(frame,(1024,768))
+		path='./pics/'+str(time.time()-8*3600)+'.jpg'
+		cv2.imwrite(path,img)
+	return 0
+
+def testSerial(cfg):
+	uart = serial.Serial(cfg.port, cfg.baud)
+	while(1):
+		l = read_GPS(uart)
+		print(l)
+
+
+def parseArguments():
+	parser = argparse.ArgumentParser(description='Record video & GPS information')
+
+	parser.add_argument('--act', default='record', help='Action type (record, testSerial')
+
+	parser.add_argument('--port', default='/dev/ttyUSB0', help='GPS port (eg: /dev/ttyUSB0)')
+	parser.add_argument('--baud', default=115200, help='Serial port baud rate')
+
+	parser.add_argument('--videoDevice', default=0, help='Video device number')
+
+	df = time.strftime('data_%Y%m%d-%H%M%s')
+	parser.add_argument('--dataPrefix', default=df, help='Stored data prefix')
+
+	args = parser.parse_args()
+	return args
+
+if __name__=='__main__':
+	
+	cfg = parseArguments()
+
+	if( cfg.act == 'testSerial' ):
+		testSerial(cfg)
+	if( cfg.act == 'record' ):
+		gps = serial.Serial(cfg.port,cfg.baud)
+		videoCapture = cv2.VideoCapture(cfg.videoDevice)
+		
+		t1 = threading.Thread(target = captureVideo, args = (videoCapture,))
+		t2 = threading.Thread(target = dataToTxt, args = (cfg,))
+
+		t1.deamon = True
+		
+		t1.start()
+		t2.start()
+
+		t2.join()
+		videoCapture.release()
