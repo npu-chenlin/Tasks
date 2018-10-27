12f6510fb351be82ebfa0ca08505f5804aeef1e8
npu-chenlin <444267416@qq.com>
2018-10-27 10:01:33 +0800

    add something

--- cpp/opencv/core/contrastandbright.cpp
+++ cpp/opencv/core/contrastandbright.cpp
@@ -15,11 +15,16 @@ int main(int argc,char** argv)
     Mat tempImg = imread("../core/1.jpg");
     Mat srcImg;
     resize(tempImg,srcImg,Size(640,480));
-    createTrackbar("Contrast","img",&contrast,)
+    createTrackbar("Contrast","img",&contrast,500,contrastandbright,&srcImg);
+    createTrackbar("bright","img",&bright,255,contrastandbright,&srcImg);
     waitKey(0);
     return 0;
 }
-void contrastandbright(int,void*)
+void contrastandbright(int value,void* img)
 {
-
+    Mat im = *(Mat*) img;
+    for(int i = 0 ; i<im.rows ; i++)
+        for(int j = 0 ; j<im.cols ; j++)
+            for(int k = 0 ; k < 3;k++)
+                im.at<Vec3b>(i,j)[k] = stuc
 }
